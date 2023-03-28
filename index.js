9f6722bdb83962f0728978c8a12e1217710d9a6f
dennis kimani <denniskimani416@gmail.com>
2023-03-28 23:15:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+// Remove main element with id "main"
+document.querySelector('#main').remove();
+
+// Create new header element with id "victory"
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Set the inner text of the new header element to "YOUR-NAME is the champion"
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the new header element to the body of the document
+document.querySelector('body').appendChild(newHeader);
